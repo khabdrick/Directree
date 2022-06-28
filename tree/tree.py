71f3394c02b936998943cb2c79ee7329e0737678
Muhammed Ali <muhamzyali@gmail.com>
2022-06-28 13:26:02 +0100

    develop the tree body

--- tree/tree.py
+++ tree/tree.py
@@ -14,7 +14,16 @@ class DirectoryTree:
     def __init__(self, root_dir):
         self._generator = _TreeGenerator(root_dir)
 
-    def generate(self):
-        tree = self._generator.build_tree()
-        for entry in tree:
-            print(entry)
+    def _tree_body(self, directory, prefix=""):
+        """Generate directory tree diagram"""
+        entries = directory.iterdir()
+        entries = sorted(entries, key=lambda entry: entry.is_file())
+        entries_count = len(entries)
+        for index, entry in enumerate(entries):
+            connector = ELBOW if index == entries_count - 1 else TEE
+            if entry.is_dir():
+                self._add_directory(
+                    entry, index, entries_count, prefix, connector
+                )
+            else:
+                self._add_file(entry, prefix, connector)
