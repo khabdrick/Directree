e912918ddbc7b8c1a2edf89663e3b3b87be18de0
Muhammed Ali <muhamzyali@gmail.com>
2022-06-28 12:56:06 +0100

    initialise characters

---
+++ tree/tree.py
@@ -0,0 +1,11 @@
+"""This module provides Directree main module."""
+
+import os
+import pathlib
+
+# initialise characters that is used to visualizethe directory tree
+PIPE = "┃"
+L = "┗━━"
+HAMMER = "┣━━"
+PIPE_SPACE = "┃   "
+SPACE = "    "
