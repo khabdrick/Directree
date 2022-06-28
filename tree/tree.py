015227d4b695b626999f1efc0af4ab14c59ccbc1
Muhammed Ali <muhamzyali@gmail.com>
2022-06-28 13:04:08 +0100

    added leafs to the tree

--- tree/tree.py
+++ tree/tree.py
@@ -5,7 +5,16 @@ import pathlib
 
 # initialise characters that is used to visualizethe directory tree
 PIPE = "┃"
-L = "┗━━"
-HAMMER = "┣━━"
+L = "┗━ 🌿"
+HAMMER = "┣━ 🌿"
 PIPE_SPACE = "┃   "
 SPACE = "    "
+
+class DirectoryTree:
+    def __init__(self, root_dir):
+        self._generator = _TreeGenerator(root_dir)
+
+    def generate(self):
+        tree = self._generator.build_tree()
+        for entry in tree:
+            print(entry)
