a8cacde131228652f67f27c2399880fb6504f032
Muhammed Ali <muhamzyali@gmail.com>
2022-06-28 13:26:32 +0100

    the generator class

--- tree/tree.py
+++ tree/tree.py
@@ -12,7 +12,28 @@ SPACE = "    "
 
 class DirectoryTree:
     def __init__(self, root_dir):
-        self._generator = _TreeGenerator(root_dir)
+        self._tree_generator = _Generator(root_dir)
+
+    def generate_tree(self):
+        tree = self._tree_generator.grow_tree()
+        for data in tree:
+            print(data)
+
+class _Generator:
+    """Generates the Directory tree"""
+    def __init__(self, root_dir):
+        self._root_dir = pathlib.Path(root_dir)
+        self._tree = []
+
+    def grow_tree(self):
+        self._tree_head()
+        self._tree_body(self._root_dir)
+        return self._tree
+
+    def _tree_head(self):
+        """Generate directory tree head"""
+        self._tree.append(f"{self._root_dir}{os.sep}")
+        self._tree.append(PIPE)
 
     def _tree_body(self, directory, prefix=""):
         """Generate directory tree diagram"""
