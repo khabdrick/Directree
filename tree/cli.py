f199407027a4cc8851091b24494da4cf13626596
Muhammed Ali <muhamzyali@gmail.com>
2022-06-29 15:43:14 +0100

    wrote the cli code

---
+++ tree/cli.py
@@ -0,0 +1,32 @@
+import argparse
+import pathlib
+import sys
+
+from . import __version__
+from .tree import Directree
+
+def main():
+    args = parse_cmd_line_arguments()
+    root_dir = pathlib.Path(args.root_dir)
+    if not root_dir.is_dir():
+        print("The specified root directory doesn't exist")
+        sys.exit()
+    tree = Directree(root_dir)
+    tree.generate_tree()
+
+def parse_cmd_line_arguments():
+    parser = argparse.ArgumentParser(
+        prog="tree",
+        description="Directree is a directory tree generator",
+        epilog="Good luck!",
+    )
+    parser.version = f"Directree v{__version__}"
+    parser.add_argument("-v", "--version", action="version")
+    parser.add_argument(
+        "root_dir",
+        metavar="ROOT_DIR",
+        nargs="?",
+        default=".",
+        help="Generate a full directory tree starting at ROOT_DIR",
+    )
+    return parser.parse_args()
